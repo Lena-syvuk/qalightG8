2fa508d233ead614ae28c60ebdcdcb73fc8e51df
Lena-syvuk <syvykolena@gmail.com>
2023-10-03 22:00:42 +0300

    commit message v1

--- HW_Scroll_pagination/page_rozetka.py
+++ HW_Scroll_pagination/page_rozetka.py
@@ -10,17 +10,21 @@ class PageRozetkaNotebooks:
     def __init__(self, driver: WebDriver):
         self.driver = driver
         self.goods_card_loc = (By.XPATH, '//li[contains(@class, "catalog-grid__cell")]')
-        self.paginator_page_loc = (By.XPATH, '//ul[starts-with(@class, "pagination")]//a[contains(@href, "notebooks/c80004/page=7/")]')
+        self.paginator_page_xpath = '//ul[starts-with(@class, "pagination")]//a[contains(@href, "page={}")]'
         self.load_more_loc = (By.XPATH, '//a[@class="show-more show-more--horizontal"]')
 
     def open(self):
         self.driver.get(self.URL)
 
+    def choose_page(self, page_num):
+        element = self.driver.find_element(By.XPATH,self.paginator_page_xpath.format(page_num))
+        return element
+
     def get_goods_count(self) -> int:
         elements = self.driver.find_elements(*self.goods_card_loc)
         return len(elements)
 
-    def is_scroll_and_click_load_more(self):
+    def scroll_and_click_load_more(self):
         count = 3
         while count >1:
             element = self.driver.find_element(*self.load_more_loc)
@@ -29,15 +33,15 @@ class PageRozetkaNotebooks:
             count -= 1
             time.sleep(3)
 
-    def is_scroll_and_choose_page_7(self):
-        element = self.driver.find_element(*self.paginator_page_loc)
+    def scroll_and_choose_page(self, page_num):
+        element = self.choose_page(page_num)
         ActionChains(self.driver).move_to_element(element).perform()
         element.click()
         time.sleep(5)
 
-    def validation_of_page(self):
+    def is_page_number_correct(self, pag_num):
         current_url = self.driver.current_url
-        return current_url == 'https://rozetka.com.ua/ua/notebooks/c80004/page=7/'
+        return current_url == f'https://rozetka.com.ua/ua/notebooks/c80004/page={pag_num}/'
 
 
 
--- HW_Scroll_pagination/test_rozetka.py
+++ HW_Scroll_pagination/test_rozetka.py
@@ -7,15 +7,15 @@ class TestRozetkaScrolling:
         expected_goods_cnt = 180
         page = PageRozetkaNotebooks(chrome)
         page.open()
-        page.is_scroll_and_click_load_more()
+        page.scroll_and_click_load_more()
         amount = page.get_goods_count()
         assert amount == expected_goods_cnt
 
-    def test_scroll_and_chsoose_page_7(self,chrome):
+    def test_scroll_and_chsoose_page(self,chrome):
         page = PageRozetkaNotebooks(chrome)
         page.open()
-        page.is_scroll_and_choose_page_7()
-        assert page.validation_of_page()
+        page.scroll_and_choose_page('7')
+        assert page.is_page_number_correct('7')
 
 
 
